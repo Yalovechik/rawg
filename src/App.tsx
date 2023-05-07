411bfb57aa1941602e787f2b8d1eb6c1123bcc94
Ivan Yalovets <yalovechik2012@gmail.com>
2023-05-07 09:10:07 +0300

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
 
   return (
     <div>
+      <Grid templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"` //large 1024px
+      }}>
+        <GridItem area="nav" bg="blue">Nav</GridItem>
+        <Show above='lg'>
+        <GridItem area="aside" bg="coral">Aside</GridItem>
+        </Show>
+        <GridItem area="main" bg="yellow">Main</GridItem>
+
+      </Grid>
+      
     </div>
   )
 }
